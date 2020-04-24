0aeb8fffb0b1e1c794657d6041e3eab65b36c5af
Surya Kumaran <suryakumaran@hrapp.co>
2020-04-24 18:05:17 +0530

    added common path for permissions

--- src/webpack-bin-permissions.js
+++ src/webpack-bin-permissions.js
@@ -1,29 +1,37 @@
-const fs = require('fs-extra')
+const fs = require("fs-extra");
 
 function WebpackBinPermission(options) {
-    this.options = options || {};
+  this.options = options || {};
 }
 
-WebpackBinPermission.prototype.apply = function (compiler) {
-    compiler.plugin('done', () => {
-        const permissions = this.options.permissions || '755';
-        const binPath = `${compiler.outputPath}/bin`;
-        const sharedlibpath = `${compiler.outputPath}/sharedlib`;
-        fs.readdir(binPath, (err, items) => {
-            if (items && items.length > 0) {
-                for (let i = 0; i < items.length; i += 1) {
-                    fs.chmodSync(`${binPath}/${items[i]}`, permissions);
-                }
-            }
-        });
-        fs.readdir(sharedlibpath, (err, items) => {
-            if (items && items.length > 0) {
-                for (let i = 0; i < items.length; i += 1) {
-                    fs.chmodSync(`${sharedlibpath}/${items[i]}`, permissions);
-                }
-            }
-        });
+WebpackBinPermission.prototype.apply = function(compiler) {
+  compiler.plugin("done", () => {
+    const permissions = this.options.permissions || "755";
+    const binPath = `${compiler.outputPath}/bin`;
+    const sharedlibpath = `${compiler.outputPath}/sharedlib`;
+    const commonpath = `${compiler.outputPath}/common`;
+    fs.readdir(binPath, (err, items) => {
+      if (items && items.length > 0) {
+        for (let i = 0; i < items.length; i += 1) {
+          fs.chmodSync(`${binPath}/${items[i]}`, permissions);
+        }
+      }
     });
+    fs.readdir(sharedlibpath, (err, items) => {
+      if (items && items.length > 0) {
+        for (let i = 0; i < items.length; i += 1) {
+          fs.chmodSync(`${sharedlibpath}/${items[i]}`, permissions);
+        }
+      }
+    });
+    fs.readdir(commonpath, (err, items) => {
+      if (items && items.length > 0) {
+        for (let i = 0; i < items.length; i += 1) {
+          fs.chmodSync(`${commonpath}/${items[i]}`, permissions);
+        }
+      }
+    });
+  });
 };
 
 module.exports = WebpackBinPermission;
